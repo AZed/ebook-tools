f64e568be419a59b07b7fac2d16d51dd56bd13c8
zed <devnull@localhost>
2008-10-07 03:24:55 +0000

    * Merge remaining user scripts into ebook.pl * Blank ebook creation partially handled

--- ebook.pl
+++ ebook.pl
@@ -2,7 +2,7 @@
 use warnings; use strict;
 
 use File::Basename 'fileparse';
-use EBook::Tools qw(split_metadata system_tidy_xml);
+use EBook::Tools qw(split_metadata system_tidy_xhtml system_tidy_xml);
 use Getopt::Long;
 
 
@@ -11,14 +11,15 @@ use Getopt::Long;
 #####################################
 
 my %opt = (
-    'dir'     => '',
-    'help'    => 0,
-    'mobi'    => 0,
-    'oeb12'   => 0,
-    'opf20'   => 0,
-    'opffile' => '',
-    'tidycmd' => '',
-    'verbose' => 0,
+    'dir'        => '',
+    'help'       => 0,
+    'mobi'       => 0,
+    'oeb12'      => 0,
+    'opf20'      => 0,
+    'opffile'    => '',
+    'tidycmd'    => '',
+    'tidysafety' => 1,
+    'verbose'    => 0,
     );
 
 GetOptions(
@@ -30,6 +31,7 @@ GetOptions(
     "opf20",
     "opffile|opf=s",
     "tidycmd",
+    'tidysafety|ts=i',
     'verbose|v+',
     );
 
@@ -42,8 +44,13 @@ if($opt{oeb12} && $opt{opf20})
 # Default to OEB12 if neither format is specified
 if(!$opt{oeb12} && !$opt{opf20}) { $opt{oeb12} = 1; }
 
+$EBook::Tools::debug = $opt{verbose};
+$EBook::Tools::tidysafety = $opt{tidysafety};
 
 my %dispatch = (
+    'adddoc'    => \&adddoc,
+    'additem'   => \&additem,
+    'blank'     => \&blank,
     'fix'       => \&fix,
     'metasplit' => \&metasplit,
     'genepub'   => \&genepub,
@@ -73,6 +80,24 @@ $dispatch{$cmd}(@ARGV);
 ########## COMMAND SUBROUTINES ##########
 #########################################
 
+sub blank
+{
+    my ($opffile) = @_;
+    my $ebook;
+
+    $opffile = $opt{opffile} if(!$opffile);
+    if(!$opffile)
+    {
+        print "You must specify an OPF filename to start with.\n";
+        exit(10);
+    }
+
+    $ebook = EBook::Tools->new();
+    $ebook->init_blank($opffile);
+    $ebook->save;
+    exit(0);
+}
+
 sub fix
 {
     my ($opffile) = @_;
@@ -82,110 +107,126 @@ sub fix
     my $tidyfile;
     my $warncount;
 
-    # If no OPF file was specified, attempt to find one
-    if(! $opffile)
-    {
-        if(-f 'META-INF/container.xml')
-        {
-            $opffile = get_container_rootfile();
-            if(! -f $opffile) { undef $opffile; }
-        }
-    }
-    if(! $opffile)
-    {
-        my @candidates = glob("*.opf");
-        die("No OPF file specified, and there are multiple files to choose from!")
-            if(scalar(@candidates) > 1);
-        die("No OPF file specified, and I couldn't find one nearby!\n")
-            if(scalar(@candidates) < 1);
-        $opffile = $candidates[0];
-    }
-    
-    die("The specified file '",$opffile,"' does not exist or is not a regular file!\n")
-        if(! -f $opffile);
-    
-    ($filebase,$filedir,$fileext) = fileparse($opffile,'\.\w+$');
-    $tidyfile = $filebase . "-tidy" . $fileext;
-    
-    # An initial cleanup is required to deal with any entities
-    $retval = system_tidy_xml($opffile,$tidyfile);
-    die ("Errors found while cleaning up '",$opffile,"' for parsing",
-         " -- look in '",$tidyfile,"' for details") if($retval > 1);
-    
-    $ebook = EBook::Tools->new($opffile);
-    $ebook->fix_oeb12;
+    $opffile = $opt{opffile} if(!$opffile);
+    $ebook = EBook::Tools->new();
+    if($opffile) { $ebook->init($opffile); }
+    else { $ebook->init(); }
+    $ebook->fix_oeb12 if($opt{oeb12});
+    $ebook->fix_opf20 if($opt{opf20});
     $ebook->fix_misc;
+    $ebook->fix_mobi if($opt{mobi});
     $ebook->save;
     
     if($ebook->errors)
     {
         $ebook->print_errors;
         print "Unrecoverable errors while fixing '",$opffile,"'!\n";
-        exit(10);
+        exit(scalar(11));
     }
     
-    $ebook->print_warnings;
-    $warncount = scalar(@{$ebook->warnings});
-    
-    $retval = system_tidy_xml($opffile,$tidyfile);
-    die ("Errors found during final cleanup of '",$opffile,"'",
-         " -- look in '",$tidyfile,"' for details") if($retval > 1);
-    
+    $ebook->print_warnings if($ebook->warnings);
     exit(0);
 }
 
 
 sub metasplit
 {
-    my ($infile) = @_;
+    my ($infile,$opffile) = @_;
     if(!$infile) { die("You must specify a file to parse.\n"); }
 
-    my $metafile;
-    my $opffile;
+    my ($filebase,$filedir,$fileext) = fileparse($infile,'\.\w+$');
+    
+    $opffile = $opt{opffile} if(!$opffile);
+    $opffile = $filebase . ".opf" if(!$opffile);
+
     my $ebook;
-    my ($filebase,$filedir,$fileext);
 
-    $metafile = split_metadata($infile);
+    split_metadata($infile,$opffile);
 
-    if(!$metafile)
+    if(!$opffile)
     {
         print {*STDERR} "No metadata block was found in '",$infile,"'\n";
         exit(20);
     }
-    
-    ($filebase,$filedir,$fileext) = fileparse($metafile,'\.\w+$');
-    $opffile = $filebase . ".opf";
-    rename($metafile,$opffile);
-    
+
     $ebook = EBook::Tools->new($opffile);
-    $ebook->fix_oeb12;
-    $ebook->fix_misc;
+    $ebook->fix_oeb12 if($opt{oeb12});
+    $ebook->fix_opf20 if($opt{opf20});
     $ebook->fix_mobi if($opt{mobi});
     
     # The split metadata never includes manifest/spine info, so add in the
     # HTML file now
-    $ebook->add_document('item-text',$ARGV[0]);
+    $ebook->add_document($infile,'item-maintext');
+    $ebook->fix_misc;
     $ebook->save;
+
+    if($ebook->errors)
+    {
+        $ebook->print_errors;
+        $ebook->print_warnings if($ebook->warnings);
+        exit(21);
+    }
+    $ebook->print_warnings if($ebook->warnings);
+    exit(0);
+}
+
+
+sub genepub
+{
+    my ($opffile) = @_;
+    my $metastring = '';
+    my $metafile;
+    my $ebookpackage;
+    my $elem;
+    my $ebook;
+    
+    my ($filebase,$filedir,$fileext);
+    
+    if($opffile) { $ebook = EBook::Tools->new($opffile); }
+    else {$ebook = EBook::Tools->new(); $ebook->init(); }
+    $ebook->fixoeb12 if($opt{oeb12});
+    $ebook->fixopf20 if($opt{opf20});
+    $ebook->fixmisc;
+    $ebook->save;
+
+    my @manifest = $ebook->manifest_hrefs;
+
+    $ebook->gen_epub();
 
     exit(0);
 }
 
-sub genepub
-{
-    print "STUB!\n";
-    exit(255);
-}
-
 sub tidyxhtml
 {
-    print "STUB!\n";
-    exit(255);
+    my ($inputfile,$tidyfile) = @_;
+    my $retval;
+
+    if(!$inputfile)
+    {
+        print "You must specify an input file to tidy.\n";
+        exit(10);
+    }
+
+    if($tidyfile) { $retval = system_tidy_xhtml($inputfile,$tidyfile); }
+    else { $retval = system_tidy_xhtml($inputfile); }
+    exit($retval);
 }
+
 
 sub tidyxml
 {
-    print "STUB!\n";
-    exit(255);
+    my ($inputfile,$tidyfile) = @_;
+    my $retval;
+
+    if(!$inputfile)
+    {
+        print "You must specify an input file to tidy.\n";
+        exit(10);
+    }
+
+    if($tidyfile) { $retval = system_tidy_xml($inputfile,$tidyfile); }
+    else { $retval = system_tidy_xml($inputfile); }
+    exit($retval);
 }
 
 ##########
