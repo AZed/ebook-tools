95f271387e26d7d9247af873bd1b0121aaa2752d
zed <devnull@localhost>
2009-02-06 14:47:37 +0000

    * Update version to 0.4.2

--- lib/EBook/Tools/IMP.pm
+++ lib/EBook/Tools/IMP.pm
@@ -1,7 +1,7 @@
 package EBook::Tools::IMP;
 use warnings; use strict; use utf8;
 use English qw( -no_match_vars );
-use version 0.74; our $VERSION = qv("0.4.1");
+use version 0.74; our $VERSION = qv("0.4.2");
 # $Revision$ $Date$
 # $Id$
 
