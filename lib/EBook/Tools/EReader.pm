60164b8ae078d90cd1eda5fbd6e6263d5c2cb027
Zed Pobre <zed@resonant.org>
2010-03-06 15:54:38 -0500

    Prepare 0.4.7 release

--- lib/EBook/Tools/EReader.pm
+++ lib/EBook/Tools/EReader.pm
@@ -1,6 +1,6 @@
 package EBook::Tools::EReader;
 use warnings; use strict; use utf8;
-use version 0.74; our $VERSION = qv("0.4.6");
+use version 0.74; our $VERSION = qv("0.4.7");
 
 # Double-sigils are needed for lexical variables in clear print statements
 ## no critic (Double-sigil dereference)
