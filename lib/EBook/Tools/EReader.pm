36775686c7a087f7272110c6d2cdcf0bc6a27194
Zed Pobre <zed@resonant.org>
2014-09-25 14:45:07 -0400

    Tag version updates for modified files

--- lib/EBook/Tools/EReader.pm
+++ lib/EBook/Tools/EReader.pm
@@ -1,6 +1,6 @@
 package EBook::Tools::EReader;
 use warnings; use strict; use utf8;
-use version 0.74; our $VERSION = qv("0.5.0");
+use version 0.74; our $VERSION = qv("0.5.2");
 
 # Double-sigils are needed for lexical variables in clear print statements
 ## no critic (Double-sigil dereference)
